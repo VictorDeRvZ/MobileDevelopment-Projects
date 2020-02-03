1e293db86504919058b7c032200eaf9bd194639d
Victor <victorp023@gmail.com>
2020-02-03 18:31:00 +0100

    PictureQuiz App example

---
+++ PictureQuiz/app/src/main/java/com/example/picturequiz/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.picturequiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main) // set activity layour resource file
+        // use id in layout file to access components
+        // set action onclock
+        btnConfirm.setOnClickListener {
+            checkAnswer()
+        }
+    }
+
+
+    private fun checkAnswer() {
+        val answer = etAnswer.text.toString()
+
+        // When the answer equals "giraffe" then display a correct message using a toast message.
+        // Otherwise display an incorrect message.
+
+        if (answer == getString(R.string.giraffe)) {
+            Toast.makeText(
+                this, getString(R.string.correct),
+                Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(this, getString(R.string.incorrect),
+                Toast.LENGTH_LONG).show()
+        }
+    }
+}
