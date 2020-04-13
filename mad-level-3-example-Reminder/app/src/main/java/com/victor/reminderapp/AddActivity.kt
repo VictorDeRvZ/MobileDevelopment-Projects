f81fc08cc8af5e84934975ca5ee8cddd5aabecdc
Victor <victorp023@gmail.com>
2020-04-13 16:56:12 +0200

    make parcelizable

--- mad-level-3-example-Reminder/app/src/main/java/com/victor/reminderapp/AddActivity.kt
+++ mad-level-3-example-Reminder/app/src/main/java/com/victor/reminderapp/AddActivity.kt
@@ -8,6 +8,7 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 
 import kotlinx.android.synthetic.main.activity_add.*
+import kotlinx.android.synthetic.main.content_add.*
 
 const val EXTRA_REMINDER = "EXTRA_REMINDER"
 
