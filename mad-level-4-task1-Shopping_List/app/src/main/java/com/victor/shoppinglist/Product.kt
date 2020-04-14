7722871139cd6eefb4611a7e565799766215732d
Victor <victorp023@gmail.com>
2020-04-14 11:54:48 +0200

    parcelable product class with annotated DB

---
+++ mad-level-4-task1-Shopping_List/app/src/main/java/com/victor/shoppinglist/Product.kt
@@ -0,0 +1,22 @@
+package com.victor.shoppinglist
+
+import android.os.Parcelable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+@Entity(tableName = "productTable")
+data class Product(
+
+@PrimaryKey(autoGenerate = true)
+@ColumnInfo(name = "name")
+var name : String,
+
+
+@ColumnInfo(name = "quantity")
+var quantity: Int
+
+
+) : Parcelable
